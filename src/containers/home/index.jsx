bf173cdd4c7bc21abdf5146a1320711b94f4a4f9
Sazith <sazith.simecsoftdev@gmail.com>
2023-11-30 10:27:28 +0600

    Hire me Button and Animation added

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 const Home = () => {
+  const navigate = useNavigate();
+  const handleNavigateToContactPage = () => {
+    navigate("/contact");
+  };
+
   return (
     <section id="home" className="home">
       <div className="home__text-wrapper">
@@ -10,6 +17,21 @@ const Home = () => {
           Web Application Developer
         </h1>
       </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleNavigateToContactPage}>Hire Me</button>
+        </div>
+      </Animate>
     </section>
   );
 };
